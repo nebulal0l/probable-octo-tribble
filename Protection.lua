b3badbdb9ba836fec153130afb7be55119f9940f
nebula <chase12hodgdon@gmail.com>
2025-05-25 02:16:04 -0300

    Create Protection.lua

---
+++ Protection.lua
@@ -0,0 +1,15 @@
+while wait() do
+    game.Players.LocalPlayer.PlayerGui.GUI_Scorecard.Scorecard.Scrolling.Visible = false
+
+    game.Workspace.KillFeed["1"].Killer.Value = "china.cs"
+    game.Workspace.KillFeed["2"].Killer.Value = "china.cs"
+    game.Workspace.KillFeed["3"].Killer.Value = "china.cs"
+    game.Workspace.KillFeed["4"].Killer.Value = "china.cs"
+    game.Workspace.KillFeed["5"].Killer.Value = "china.cs"
+    game.Workspace.KillFeed["6"].Killer.Value = "china.cs"
+
+    game.Players.LocalPlayer.PlayerGui.GUI.Winner.Visible = false
+    game.Players.LocalPlayer.PlayerGui.Menew_Main.Container.PlrName.Text = "china.cs ☭"
+
+    game.Players.LocalPlayer.PlayerGui.GUI_Scorecard.Scorecard.PlayerCard.Username.Text = "join china.cs — https://discord.gg/c6wcdmj8J3"
+end
